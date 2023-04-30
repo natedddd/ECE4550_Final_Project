69d3079c705f333ecd708ac87d5b54f1bfd73ed0
Nate Doggett <nate.doggett8@gmail.com>
2023-04-30 10:56:19 -0400

    Add starter code from Proj 4. Has RM & DM implemented

--- scheduler.h
+++ scheduler.h
@@ -28,9 +28,12 @@ extern "C" {
 
 /* The scheduling policy can be chosen from one of these. */
 #define schedSCHEDULING_POLICY_RMS 1 		/* Rate-monotonic scheduling */
+#define schedSCHEDULING_POLICY_DMS 2 		/* Deadline-monotonic scheduling */
 
 /* Configure scheduling policy by setting this define to the appropriate one. */
 #define schedSCHEDULING_POLICY schedSCHEDULING_POLICY_RMS //schedSCHEDULING_POLICY_EDF
+
+#define schedAUGMENT_SCHEDULER_TASK 0       /* Augment the scheduler to add scheduler overhead*/
 
 /* Maximum number of periodic tasks that can be created. (Scheduler task is
  * not included) */
@@ -59,7 +62,7 @@ extern "C" {
 	/* Stack size of the scheduler task. */
 	#define schedSCHEDULER_TASK_STACK_SIZE 200 
 	/* The period of the scheduler task in software ticks. */
-	#define schedSCHEDULER_TASK_PERIOD pdMS_TO_TICKS( 100 )	
+	#define schedSCHEDULER_TASK_PERIOD pdMS_TO_TICKS( 100 )
 #endif /* schedUSE_SCHEDULER_TASK */
 
 /* This function must be called before any other function call from scheduler.h. */
